e84076e4ebf843eb2cc715fc752c0c8f2dbc36ad
GitHub Action <action@github.com>
2024-07-30 07:22:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/lymansix/do2sql/MyBundle.kt
+++ src/main/kotlin/com/github/lymansix/do2sql/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lymansix.do2sql
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/lymansix/do2sql/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lymansix/do2sql/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.lymansix.do2sql.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lymansix.do2sql.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
